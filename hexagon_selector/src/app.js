088f0895534e6b24a9898a982066251355e35096
carstonhernke <carston17@gmail.com>
2022-12-07 00:12:26 +0100

    adding hex selector

--- hexagon_selector/src/app.js
+++ hexagon_selector/src/app.js
@@ -3,12 +3,14 @@ import * as ReactDOM from 'react-dom/client';
 import React, {useState} from 'react';
 import DeckGL from '@deck.gl/react';
 import {H3HexagonLayer} from '@deck.gl/geo-layers';
-import Map from 'react-map-gl';
+import {Map as MapGL} from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
-import { EditableGeoJsonLayer, DrawPolygonMode } from 'nebula.gl';
+import { EditableGeoJsonLayer } from '@nebula.gl/layers';
+import { DrawPolygonMode, ModifyMode, TranslateMode, DuplicateMode, ViewMode } from "@nebula.gl/edit-modes";
 import * as turf from "@turf/turf";
-import {polygonToCells} from "h3-js";
+import {polygonToCells, getHexagonAreaAvg, UNITS} from "h3-js";
 import Sidebox from './components/sidebox';
+import { Position, Toaster } from "@blueprintjs/core";
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
@@ -39,6 +41,14 @@ const MAP_STYLE = {
 ]
 };
 
+// map of edit modes
+var editModes = new Map();
+editModes.set('ViewMode', ViewMode);
+editModes.set('DrawPolygonMode', DrawPolygonMode);
+editModes.set('ModifyMode', ModifyMode);
+editModes.set('TranslateMode', TranslateMode);
+editModes.set('DuplicateMode', DuplicateMode);
+
 const layers = []
 
 // Data to be used by the Hex Layer
@@ -46,75 +56,75 @@ const hexData = [
   ];
 
 function convertToHexagons(geo, resolution) {
-    if(geo['features'].length == 0){
-        return []
-    }
-    var outputArray = []
-    geo['features'].forEach((element, index) => {
-        var cells = polygonToCells(element['geometry']['coordinates'], resolution, true)
-        cells.forEach((element, index) => {
-            var newElement = {
-                hex: element,
-                count: 1,
-            };
-            outputArray.push(newElement) ;
-          });
-      })    
-    return outputArray
+  if(geo['features'].length == 0){
+      return []
+  }
+  // check if the number of hexagons is expected to exceed 200k
+  if ((turf.area(geo) / getHexagonAreaAvg(resolution, UNITS.m2)) > 200000){
+    alert("Too many hexagons!")
+    return
+  }
+  var outputArray = []
+  geo['features'].forEach((element, index) => {
+      var cells = polygonToCells(element['geometry']['coordinates'], resolution, true)
+      cells.forEach((element, index) => {
+          outputArray.push(element) ;
+        });
+    })    
+  return [...new Set(outputArray)]
 }
 
-function getArea(geo){
-    var area = (turf.area(geo)/1000000).toFixed(2)
-    return area
+function getArea(geo) {
+  var area = (turf.area(geo)/1000000).toFixed(2)
+  return area
+}
+
+function showHexNumberWarning() {
+  alert("Attempting to render too many hexbins. Create a smaller polygon or use a smaller resolution")
 }
 
 const emptyGeoJsonData = {
-    "type": "FeatureCollection",
-    "features": []
-  }
+  "type": "FeatureCollection",
+  "features": []
+}
 
-const selectedFeatureIndexes = [];
 
 function App() {
     const [geoData, setGeoData] = useState(emptyGeoJsonData);
-    const [area, setArea] = useState('N/A');
-    const [h3Level, setH3Level] = useState(5)
+    const [editModeName, setEditModeName] = useState("DrawPolygonMode");
+    const [selectedFeatureIndexes, setSelectedFeatureIndexes] = useState([0]);
+    const [hexagons, setHexagons] = useState([]);
+    const [h3Level, setH3Level] = useState(4)
 
     const editableLayer = new EditableGeoJsonLayer({
-        id: 'geojson-layer',
         data: geoData,
-        mode: DrawPolygonMode,
-        selectedFeatureIndexes,
+        mode: editModes.get(editModeName),
+        selectedFeatureIndexes: selectedFeatureIndexes,
     
-        onEdit: ({ editType, updatedData }) => {
-            if(editType == 'addFeature'){
-                console.log(`event: ${editType}`)
-                console.log(`area: ${area}`)
-                setGeoData(updatedData)
-            }
+        onEdit: ({ updatedData }) => {
+            setGeoData(updatedData)
+            setHexagons(convertToHexagons(updatedData, h3Level))
         },
         });
     
     const hexagonLayer = new H3HexagonLayer({
         id: 'h3-hexagon-layer',
-        data: convertToHexagons(geoData, h3Level),
+        data: hexagons,
         pickable: true,
         filled: true,
         extruded: false,
         elevationScale: 20,
-        dataComparator: (oldData, newData) => {
-            console.log(oldData.length == newData.length)
-            return oldData.length == newData.length},
-        getHexagon: d => d.hex,
-        getFillColor: d => [255, 100, 0],
-        getLineWidth: (1/h3Level)*200,
-        getLineColor: d => [255, 255, 255],
-        getElevation: d => 0
+        lineWidthUnits: 'pixels',
+        getHexagon: d => d,
+        getFillColor: [255, 100, 0],
+        getLineWidth: 1,
+        getLineColor: [255, 255, 255],
+        getElevation: 0
         });
 
     const layers = [
-        editableLayer,
-        hexagonLayer
+        hexagonLayer,
+        editableLayer
     ];
 
     return (
@@ -124,13 +134,33 @@ function App() {
                     initialViewState={INITIAL_VIEW_STATE}
                     controller={true}
                     layers={layers}
-                    getTooltip={({object}) => object && `${object.hex} count: ${object.count}`}
+                    onClick={(info) => {
+                      if (editModes.get(editModeName) === ViewMode)
+                        if (info) {
+                          setSelectedFeatureIndexes([info.index]);
+                        } else {
+                          setSelectedFeatureIndexes([]);
+                        }
+                    }}
                     >
-                    <Map mapStyle={MAP_STYLE} mapLib={maplibregl}/>
+                    <MapGL mapStyle={MAP_STYLE} mapLib={maplibregl}/>
                 </DeckGL>
             </div>
             <div className = "sidebox">
-                <Sidebox area={getArea(geoData)} hexagonCount = {convertToHexagons(geoData, h3Level).length} h3Level = {h3Level} setH3Level = {setH3Level} setGeoData = {setGeoData}/>
+                <Sidebox 
+                  area={getArea(geoData)} 
+                  geoData= {geoData}
+                  hexagons = {hexagons} 
+                  setHexagons = {setHexagons} 
+                  setGeoData = {setGeoData} 
+                  editModeName = {editModeName} 
+                  setEditModeName = {setEditModeName} 
+                  h3Level = {h3Level} 
+                  setH3Level = {setH3Level} 
+                  convertToHexagons = {convertToHexagons}
+                  setSelectedFeatureIndexes = {setSelectedFeatureIndexes}
+                  showHexNumberWarning = {showHexNumberWarning}
+                />
             </div>
         </div>
     );
--- hexagon_selector/src/components/sidebox.js
+++ hexagon_selector/src/components/sidebox.js
@@ -1,34 +1,111 @@
 import React, {useState} from 'react';
-import { Button, Slider } from "@blueprintjs/core";
+import { Button, Slider, Menu, MenuDivider, MenuItem, Icon } from "@blueprintjs/core";
+import * as turf from "@turf/turf";
+import {getHexagonAreaAvg, UNITS} from "h3-js";
 
 const emptyGeoJsonData = {
     "type": "FeatureCollection",
     "features": []
-  }
+}
+
+function handleSliderChange(selectedLevel, geoData, setH3Level, setHexagons, convertToHexagons,showHexNumberWarning) {
+    // check if slider will result in too many hexagons
+    if ((turf.area(geoData) / getHexagonAreaAvg(selectedLevel, UNITS.m2)) > 200000){
+        //setH3Level(4)
+        //setHexagons(convertToHexagons(geoData,4))
+        return
+      }
+    else {
+        setH3Level(selectedLevel)
+        setHexagons(convertToHexagons(geoData,selectedLevel))
+    }
+}
+
+function handleTrashButtonClick(emptyGeoJsonData, setGeoData, setHexagons, setSelectedFeatureIndexes){
+    setGeoData(emptyGeoJsonData)
+    setHexagons([])
+    setSelectedFeatureIndexes([0])
+}
 
 export default function Sidebox(props){
       return (
-        <div>
+        <div className="sideboxContent">
           <div className="title">Polygon to H3 Converter</div>
           <div className="optionBox">
             <div className="subtitle">Options</div>
-            <Button icon="upload" text="Upload GeoJSON"/>
-            <Button icon="trash" text="Reset" onClick={() => props.setGeoData(emptyGeoJsonData)}/>
+            <Menu className='menu'>
+                <MenuDivider title="Editor Mode" />
+                <MenuItem 
+                    icon="select"
+                    text="Select Polygon" 
+                    selected={props.editModeName == "ViewMode"}
+                    onClick={() => props.setEditModeName("ViewMode")}
+                />  
+                <MenuItem 
+                    icon="new-object"
+                    text="Add Polygon" 
+                    selected={props.editModeName == "DrawPolygonMode"}
+                    onClick={() => props.setEditModeName("DrawPolygonMode")}
+                />   
+                <MenuItem 
+                    icon="edit"
+                    text="Edit Points" 
+                    selected={props.editModeName == "ModifyMode"}
+                    onClick={() => props.setEditModeName("ModifyMode")}
+                />  
+                <MenuItem 
+                    icon="move"
+                    text="Move Polygon" 
+                    selected={props.editModeName == "TranslateMode"}
+                    onClick={() => props.setEditModeName("TranslateMode")}
+                />  
+                <MenuItem 
+                    icon="duplicate"
+                    text="Duplicate Polygon" 
+                    selected={props.editModeName == "DuplicateMode"}
+                    onClick={() => props.setEditModeName("DuplicateMode")}
+                />
+                <MenuDivider title="Settings" />
+                <MenuItem 
+                    icon="upload"
+                    text="Upload GeoJSON" 
+                />
+                <MenuItem 
+                    icon="trash"
+                    text="Reset Geography" 
+                    onClick={() => handleTrashButtonClick(emptyGeoJsonData, props.setGeoData, props.setHexagons, props.setSelectedFeatureIndexes)}
+                />
+            </Menu>
             <div>H3 Index Resolution</div>
             <Slider
-                        min={1}
-                        max={16}
-                        stepSize={1}
-                        labelStepSize={5}
-                        onChange={props.setH3Level}
-                        value={props.h3Level}
-                        handleHtmlProps={{ "aria-label": "example 1" }}
+                className='menu'
+                min={1}
+                max={15}
+                stepSize={1}
+                labelStepSize={5}
+                onChange={(selectedLevel) => handleSliderChange(selectedLevel,
+                                                                 props.geoData, 
+                                                                 props.setH3Level, 
+                                                                 props.setHexagons, 
+                                                                 props.convertToHexagons,
+                                                                 props.showHexNumberWarning)}
+                value={props.h3Level}
+                handleHtmlProps={{ "aria-label": "example 1" }}
                     />
           </div>
           <div className="subtitle">Output</div>
           <div className="results">Area: {props.area}km2</div>
-          <div className="results">Number of Hexagons: {props.hexagonCount}</div>
-          <Button icon="download" text="Download Hexagons"/>
+          <div className="results">Number of Hexagons: {props.hexagons.length}</div>
+            <Menu className='menu'>
+                    <MenuItem 
+                        icon="download"
+                        text="Download" 
+                        href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                            JSON.stringify(props.hexagons)
+                          )}`}
+                        download="hexagons.json"
+                    />    
+            </Menu>
         </div>
       );
   }
